af550ff647a3f341b635192745ecd48113c23864
TruncatedDinosour <71613062+TruncatedDinosour@users.noreply.github.com>
2022-08-11 06:15:13 +0000

    Create CONTRIBUTING.rs

---
+++ CONTRIBUTING.rs
@@ -0,0 +1,96 @@
+fn main() {
+    unsafe {
+        unsafe {
+        let mut ZeroCostAbstractionsAndMemorySafety = Vec::new();
+ZeroCostAbstractionsAndMemorySafety.push('#');
+ZeroCostAbstractionsAndMemorySafety.push(' ');
+ZeroCostAbstractionsAndMemorySafety.push('C');
+ZeroCostAbstractionsAndMemorySafety.push('o');
+ZeroCostAbstractionsAndMemorySafety.push('n');
+ZeroCostAbstractionsAndMemorySafety.push('t');
+ZeroCostAbstractionsAndMemorySafety.push('r');
+ZeroCostAbstractionsAndMemorySafety.push('i');
+ZeroCostAbstractionsAndMemorySafety.push('b');
+ZeroCostAbstractionsAndMemorySafety.push('u');
+ZeroCostAbstractionsAndMemorySafety.push('t');
+ZeroCostAbstractionsAndMemorySafety.push('i');
+ZeroCostAbstractionsAndMemorySafety.push('n');
+ZeroCostAbstractionsAndMemorySafety.push('g');
+ZeroCostAbstractionsAndMemorySafety.push('.');
+ZeroCostAbstractionsAndMemorySafety.push('m');
+ZeroCostAbstractionsAndMemorySafety.push('d');
+ZeroCostAbstractionsAndMemorySafety.push('\n');
+ZeroCostAbstractionsAndMemorySafety.push('\n');
+ZeroCostAbstractionsAndMemorySafety.push('\n');
+ZeroCostAbstractionsAndMemorySafety.push('I');
+ZeroCostAbstractionsAndMemorySafety.push('f');
+ZeroCostAbstractionsAndMemorySafety.push(' ');
+ZeroCostAbstractionsAndMemorySafety.push('y');
+ZeroCostAbstractionsAndMemorySafety.push('o');
+ZeroCostAbstractionsAndMemorySafety.push('u');
+ZeroCostAbstractionsAndMemorySafety.push(' ');
+ZeroCostAbstractionsAndMemorySafety.push('a');
+ZeroCostAbstractionsAndMemorySafety.push('r');
+ZeroCostAbstractionsAndMemorySafety.push('e');
+ZeroCostAbstractionsAndMemorySafety.push(' ');
+ZeroCostAbstractionsAndMemorySafety.push('m');
+ZeroCostAbstractionsAndMemorySafety.push('a');
+ZeroCostAbstractionsAndMemorySafety.push('k');
+ZeroCostAbstractionsAndMemorySafety.push('i');
+ZeroCostAbstractionsAndMemorySafety.push('n');
+ZeroCostAbstractionsAndMemorySafety.push('g');
+ZeroCostAbstractionsAndMemorySafety.push(' ');
+ZeroCostAbstractionsAndMemorySafety.push('a');
+ZeroCostAbstractionsAndMemorySafety.push(' ');
+ZeroCostAbstractionsAndMemorySafety.push('p');
+ZeroCostAbstractionsAndMemorySafety.push('r');
+ZeroCostAbstractionsAndMemorySafety.push(',');
+ZeroCostAbstractionsAndMemorySafety.push(' ');
+ZeroCostAbstractionsAndMemorySafety.push('m');
+ZeroCostAbstractionsAndMemorySafety.push('a');
+ZeroCostAbstractionsAndMemorySafety.push('k');
+ZeroCostAbstractionsAndMemorySafety.push('e');
+ZeroCostAbstractionsAndMemorySafety.push(' ');
+ZeroCostAbstractionsAndMemorySafety.push('s');
+ZeroCostAbstractionsAndMemorySafety.push('u');
+ZeroCostAbstractionsAndMemorySafety.push('r');
+ZeroCostAbstractionsAndMemorySafety.push('e');
+ZeroCostAbstractionsAndMemorySafety.push(' ');
+ZeroCostAbstractionsAndMemorySafety.push('t');
+ZeroCostAbstractionsAndMemorySafety.push('o');
+ZeroCostAbstractionsAndMemorySafety.push(' ');
+ZeroCostAbstractionsAndMemorySafety.push('a');
+ZeroCostAbstractionsAndMemorySafety.push('d');
+ZeroCostAbstractionsAndMemorySafety.push('d');
+ZeroCostAbstractionsAndMemorySafety.push(' ');
+ZeroCostAbstractionsAndMemorySafety.push('a');
+ZeroCostAbstractionsAndMemorySafety.push('t');
+ZeroCostAbstractionsAndMemorySafety.push(' ');
+ZeroCostAbstractionsAndMemorySafety.push('l');
+ZeroCostAbstractionsAndMemorySafety.push('e');
+ZeroCostAbstractionsAndMemorySafety.push('a');
+ZeroCostAbstractionsAndMemorySafety.push('s');
+ZeroCostAbstractionsAndMemorySafety.push('t');
+ZeroCostAbstractionsAndMemorySafety.push(' ');
+ZeroCostAbstractionsAndMemorySafety.push('1');
+ZeroCostAbstractionsAndMemorySafety.push(' ');
+ZeroCostAbstractionsAndMemorySafety.push('m');
+ZeroCostAbstractionsAndMemorySafety.push('o');
+ZeroCostAbstractionsAndMemorySafety.push('r');
+ZeroCostAbstractionsAndMemorySafety.push('e');
+ZeroCostAbstractionsAndMemorySafety.push(' ');
+ZeroCostAbstractionsAndMemorySafety.push('c');
+ZeroCostAbstractionsAndMemorySafety.push('r');
+ZeroCostAbstractionsAndMemorySafety.push('a');
+ZeroCostAbstractionsAndMemorySafety.push('t');
+ZeroCostAbstractionsAndMemorySafety.push('e');
+ZeroCostAbstractionsAndMemorySafety.push('\n');
+        unsafe {
+        for MemorySafety in ZeroCostAbstractionsAndMemorySafety {
+            unsafe{
+print!("{}", MemorySafety);
+            }
+        }}
+    }
+    };
+}
